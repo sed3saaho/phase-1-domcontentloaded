4c4a71ed482c58a9c7edba58f3aff053e71120ec
Sedly <sedly.keith@student.moringaschool.com>
2024-04-11 16:13:35 +0300

    phase-1-domcontentloaded

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener( "DOMContentLoaded", function () {
+    DomUpdate()
+  } );
+  
+  
+  function DomUpdate() {
+    document.getElementById( "text" )
+      .innerHTML = "This is really cool!";
+  }
